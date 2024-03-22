bb429eb71a87d07832871068122822311d75e2f5
otikdjubet <jyddinampo@gmail.com>
2024-03-22 22:07:24 +0800

    Third commit

--- SlotMachine.py
+++ SlotMachine.py
@@ -1,6 +1,64 @@
+import random
+
 MAX_LINES =3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "E": 8,
+}
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "E": 2,
+} #multiplier
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            
+    return winnings
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for col in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+    return columns
+
+def print_slot_machine(columns): #flip columns and rows
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end =" | ")
+            else:
+                print(column[row], end = "")
+        print()
 
 def deposit():
     while True:
@@ -51,8 +109,11 @@ def main():
             print(f"You dont have enough balance. Your current balance is ${balance}")
         else:
             break
+
     new_bal = balance - total_bet
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}. Remaining balance is ${new_bal}" )
 
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
