b0ff72f579c12b2f237199b226ccf26c5e40b340
otikdjubet <jyddinampo@gmail.com>
2024-03-22 21:04:24 +0800

    First commit

---
+++ SlotMachine.py
@@ -0,0 +1,16 @@
+def deposit():
+    while True:
+        amount = input("How much would you like to deposit? ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number")
+
+
+
+
+deposit()
